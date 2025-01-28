cf91d4550c2eecf1784353ce850b9ada6f8b9593
Lucas Duarte <lucs.rkt@gmail.com>
2025-01-28 10:35:49 -0300

    feat: add base employee page

---
+++ pages/employees/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function Employees() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      Employees page
+    </Box>
+  )
+}
