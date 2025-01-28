e8ca1e76fd68e138589cd4b93de50ed7c65ac8b9
Lucas Duarte <lucs.rkt@gmail.com>
2025-01-28 10:34:16 -0300

    feat: add base layout

---
+++ components/layout.js
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import Image from 'next/image';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import BusinessIcon from '@mui/icons-material/Business';
+import Groups2Icon from '@mui/icons-material/Groups2';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import { Divider } from '@mui/material';
+import { useRouter } from 'next/router';
+
+const drawerWidth = 240;
+
+export default function Layout({ children }) {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
+
+  const router = useRouter()
+
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
+
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
+
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar sx={{ backgroundColor: '#CB90FF', display: 'flex', justifyContent: 'center' }}>
+        <Image
+          src="/download.png"
+          alt="Qulture Rocks Logo"
+          width={60}
+          height={10}
+          priority
+        />
+      </Toolbar>
+      <Divider />
+      <List>
+        {['Companhia', 'Colaboradores'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton
+              onClick={() => {
+                const path = {
+                  'Companhia': 'company',
+                  'Colaboradores': 'employees'
+                }[text]
+                router.push(`/${path}`)
+              }}
+            >
+              <ListItemIcon>
+                {index % 2 === 0 ? <BusinessIcon /> : <Groups2Icon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          backgroundColor: '#CB90FF'
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import Layout from '@/components/layout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
